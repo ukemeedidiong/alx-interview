3bf5aa478ff2fcb29308d78e891c36cb16b1430e
ukemeedidiong <ukemeedidiong226@gmail.com>
2024-06-20 23:40:26 +0100

    third commit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,69 +1,50 @@
 #!/usr/bin/python3
 """
-parsing function
+log parsing
 """
+
 import sys
-import signal
+import re
 
-counters = {
-    "size": 0,
-    "lines": 1
-}
 
-cntCode = {
-    "200": 0, "301": 0, "400": 0, "401": 0,
-    "403": 0, "404": 0, "405": 0, "500": 0
-}
-
-def printCodes():
+def output(log: dict) -> None:
     """
-    Function to print the codes and the number of occurrences
+    helper function to display stats
     """
-    # Print file size
-    print("File size: {}".format(counters["size"]))
-    # Print all codes
-    for key in sorted(cntCode.keys()):
-        # If a value is not 0
-        if cntCode[key] != 0:
-            print("{}: {}".format(key, cntCode[key]))
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
-def countCodeSize(listData):
-    """
-    Count the codes and file size
-    """
-    # Validate format
-    if len(listData) < 7:
-        return
-    # Validate request format
-    if listData[5] != '"GET' or listData[6] != '/projects/260' or listData[7] != 'HTTP/1.1"':
-        return
-    try:
-        # Count file size
-        counters["size"] += int(listData[-1])
-        # If exists the code
-        if listData[-2] in cntCode:
-            # Count status code
-            cntCode[listData[-2]] += 1
-    except ValueError:
-        pass
-
-def signal_handler(sig, frame):
-    printCodes()
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, signal_handler)
 
 if __name__ == "__main__":
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+
+    line_count = 0
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
+
     try:
         for line in sys.stdin:
-            try:
-                countCodeSize(line.split(" "))
-            except Exception:
-                pass
-            if counters["lines"] % 10 == 0:
-                printCodes()
-            counters["lines"] += 1
-    except KeyboardInterrupt:
-        printCodes()
-        raise
-    printCodes()
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if (match):
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
+
+                # File size
+                log["file_size"] += file_size
+
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
+
+                if (line_count % 10 == 0):
+                    output(log)
+    finally:
+        output(log)
